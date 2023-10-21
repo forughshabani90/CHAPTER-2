4b9e634b87a318dc5fc1e103aecec320742d778a
forughshabani90 <forugh.shabani.90@gmail.com>
2023-10-21 10:30:12 -0700

    first commit

--- src/ComputeArea.java
+++ src/ComputeArea.java
@@ -1,11 +1,12 @@
+import java.util.Scanner;
+
 public class ComputeArea {
     public static void main(String[] args) {
-        double radius;
-        double area;
-        radius=20;
-        area= radius * radius* 3.14159;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("inter a number for radius");
+        double radius = scanner.nextDouble();
+        double area = radius * radius * 3.14159;
         System.out.println(" the area for the circle of radius " + radius + "is" + area);
-
 
 
     }
