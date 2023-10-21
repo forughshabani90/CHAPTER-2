dbc178803066716180eac4284c3b3dda75ef20c2
forughshabani90 <forugh.shabani.90@gmail.com>
2023-10-21 08:42:26 -0700

    ok

--- src/ComputeArea.java
+++ src/ComputeArea.java
@@ -2,8 +2,8 @@ public class ComputeArea {
     public static void main(String[] args) {
         double radius;
         double area;
-        radiud=20;
-        area- radius * radius* 3.14159;
+        radius=20;
+        area= radius * radius* 3.14159;
         System.out.println(" the area for the circle of radius " + radius + "is" + area);
 
 
