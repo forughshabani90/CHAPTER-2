f87f5c61e9c2f1b72c980d45f09c1ae5a2cf9dae
forughshabani90 <forugh.shabani.90@gmail.com>
2023-10-21 08:54:12 -0700

    first commit

---
+++ src/Test.java
@@ -0,0 +1,13 @@
+
+
+public class Test {
+    public void main(string[] args) {
+        double i = 50.0;
+        double k = i + 50.0;
+        double j = k + 1;
+
+        System.out.println("j is " + j + " and
+                k is " + k );
+    }
+
+}
